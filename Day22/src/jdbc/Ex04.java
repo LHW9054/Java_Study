f75971ff14ecc67181b6a7345aef5a89a40419db
DEV_NangMan <135090213+LHW9054@users.noreply.github.com>
2023-12-05 14:30:27 +0900

    자바공부22일차
    자바공부22일차

---
+++ Day22/src/jdbc/Ex04.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+public class Ex04 {
+	public static void main(String[] args) throws Exception{
+		
+		System.out.println("\n\t전체목록\n\n");
+		Ex01.main(null);
+		
+		System.out.println("\n\t행 추가\n\n");
+		Ex02_Insert.main(null);
+		
+		System.out.println("\n\t6번 상품 삭제\n");
+		Ex03_Delete.main(null);
+		
+		System.out.println("\n\t6번상품삭제\n");
+		Ex01.main(null);
+	
+		
+	}
+}
